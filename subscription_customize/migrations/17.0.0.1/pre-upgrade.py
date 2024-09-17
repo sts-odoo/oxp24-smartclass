f896b71067f723884b118f5df035369bcb97ec98
Stanislas Sobieski <sts@odoo.com>
2024-09-17 14:52:10 +0200

    [MIG] upgrade scripts to 17.0

---
+++ subscription_customize/migrations/17.0.0.1/pre-upgrade.py
@@ -0,0 +1,22 @@
+import logging
+
+from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+def migrate(cr, version):
+    _logger.info('creating column from previous version')
+    util.create_column(cr, 'sale_order', 'parent_id', 'integer')
+    util.create_column(cr, 'sale_order', 'child_recurring_total', 'double precision')
+    util.create_column(cr, 'sale_order', 'amount_at_start', 'double precision')
+    _logger.info('creating column from previous version')
+
+    cr.execute('''
+        UPDATE sale_order
+            SET parent_id = sale_subscription.parent_id,
+                child_recurring_total = sale_subscription.child_recurring_total,
+                amount_at_start = sale_subscription.amount_at_start
+            FROM sale_subscription
+            WHERE sale_subscription.new_sale_order_id = sale_order.id
+    ''')
+    _logger.info('finished')
