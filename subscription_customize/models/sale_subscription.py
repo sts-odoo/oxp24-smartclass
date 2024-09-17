aa3f656714e12b1c55f14d62f128fb98df83fae1
Stanislas Sobieski <sts@odoo.com>
2024-09-17 13:17:17 +0200

    [MIG] migrate code to 17.0

--- subscription_customize/models/sale_subscription.py
+++ subscription_customize/models/sale_subscription.py
@@ -4,18 +4,19 @@ from odoo import models, api, fields
 from odoo.exceptions import ValidationError
 
 
-class SaleSubscription(models.Model):
-    _inherit = "sale.subscription"
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
 
-    parent_id = fields.Many2one('sale.subscription', string="Parent subscription")
-    child_ids = fields.One2many('sale.subscription', 'parent_id', string="Child subscriptions")
+    parent_id = fields.Many2one('sale.order', string="Parent subscription", domain=[('is_subscription', '=', True)])
+    child_ids = fields.One2many('sale.order', 'parent_id', string="Child subscriptions")
     child_recurring_total = fields.Monetary('Total recurring amount from child', compute='_compute_child_amount', store=True, readonly=True)
     amount_at_start = fields.Monetary('Amount at start up')
 
-    def start_subscription(self):
-        self.ensure_one()
-        res = super(SaleSubscription, self).start_subscription()
-        self.amount_at_start = self.recurring_total
+    def action_confirm(self):
+        res = super().action_confirm()
+        for rec in self:
+            if rec.is_subscription:
+                rec.amount_at_start = rec.recurring_total
         return res
 
     @api.depends('child_ids.recurring_total')
